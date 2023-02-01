bb18648a7ac5420d3217f85fdf4c16925bdfbb11
Paul SEREN-ROSSO <p.serenrosso@rubika-edu.com>
2023-02-01 11:39:19 +0100

    remove attribute

--- Utilities/Assets/src/Attributes/DependsOnServiceAttribute.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Attributes
-{
-    [AttributeUsage(AttributeTargets.Field, Inherited = true, AllowMultiple = false)]
-    public class DependsOnServiceAttribute : Attribute
-    {}
-}
